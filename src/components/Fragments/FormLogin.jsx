dca95bf29dc4115c4b003128b13cf14b4ed487df
Dewa Angga PC <devaangga53@gmail.com>
2024-01-07 07:50:59 +0800

    first commit

---
+++ src/components/Fragments/FormLogin.jsx
@@ -0,0 +1,14 @@
+import InputForm from '../Elements/Input/index';
+import Button from '../Elements/Button/index';
+
+const FormLogin = () => {
+  return (
+    <form action="">
+      <InputForm label="Email" type="email" placeholder="example@mail.com" name="email"></InputForm>
+      <InputForm label="Password" type="password" placeholder="******" name="passwordcd "></InputForm>
+      <Button variant="bg-blue-600 w-full">Login</Button>
+    </form>
+  );
+};
+
+export default FormLogin;
