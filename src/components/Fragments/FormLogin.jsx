932936ef86195f361f24906b046606a69f46855a
Dewa Angga <devaangga53@gmail.com>
2024-01-07 10:56:41 +0800

    hanya merapikan baris code

--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -4,8 +4,20 @@ import Button from '../Elements/Button/index';
 const FormLogin = () => {
   return (
     <form action="">
-      <InputForm label="Email" type="email" placeholder="example@mail.com" name="email"></InputForm>
-      <InputForm label="Password" type="password" placeholder="******" name="passwordcd "></InputForm>
+      <InputForm 
+      label="Email" 
+      type="email" 
+      placeholder="example@mail.com" 
+      name="email">
+      </InputForm>
+
+      <InputForm 
+      label="Password" 
+      type="password" 
+      placeholder="******" 
+      name="password">
+      </InputForm>
+
       <Button variant="bg-blue-600 w-full">Login</Button>
     </form>
   );
