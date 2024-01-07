d64df648bd519fb13918b2fbf68100812906ca39
Dewa Angga <devaangga53@gmail.com>
2024-01-07 13:07:45 +0800

    Menginstal react-router-dom dan memindahkan css di app.jsx ke auth.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import LoginPage from './pages/login';
 import RegisterPage from './pages/register';
 
 
 function App() {
   return (
-    <div className="flex justify-center min-h-screen items-center">
-      {/* <LoginPage /> */}
+    <div>
       <RegisterPage/>
     </div>
   );
