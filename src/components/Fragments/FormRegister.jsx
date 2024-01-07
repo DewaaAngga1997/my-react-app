404764698014609be5d153533f9cb7cca5d8d99a
Dewa Angga <devaangga53@gmail.com>
2024-01-07 10:17:40 +0800

    menambahkan page form register

---
+++ src/components/Fragments/FormRegister.jsx
@@ -0,0 +1,16 @@
+import InputForm from '../Elements/Input/index';
+import Button from '../Elements/Button/index';
+
+const FormRegister = () => {
+  return (
+    <form action="">
+      <InputForm label="Fullname" type="text" placeholder="insert your name here ..." name="fullname"></InputForm>
+      <InputForm label="Email" type="email" placeholder="example@mail.com" name="email"></InputForm>
+      <InputForm label="Password" type="password" placeholder="******" name="password "></InputForm>
+      <InputForm label="Confirm Password" type="password" placeholder="******" name="confirmPassword"></InputForm>
+      <Button variant="bg-blue-600 w-full">Register</Button>
+    </form>
+  );
+};
+
+export default FormRegister;
