5ffb8664db6bcb9fe8dc87f721f7a93b84e3f825
Dewa Angga PC <devaangga53@gmail.com>
2024-01-10 22:24:20 +0800

    edit teks di halaman error404.jsx

--- src/pages/error404.jsx
+++ src/pages/error404.jsx
@@ -6,9 +6,9 @@ const ErrorPage = () => {
     return (
         <div className="flex justify-center min-h-screen items-center flex-col">
             <h1 className="text-3xl font-semibold">Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
-            <p>
-                <i>{error.statusText || error.message}</i>
+            <p className="my-5 text-xl">Maaf, Halaman yang anda cari tidak ditemukan.</p>
+            <p className="text-xl">
+                {error.statusText || error.message}
             </p>
         </div>
     );
