9fe9fc1363d5cb542d2336258ce74b82fa1e0902
Dewa Angga <devaangga53@gmail.com>
2024-01-11 13:20:50 +0800

    membuat kondisi rendering di AuthLayout.jsx

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,14 +1,10 @@
-import { Link } from 'react-router-dom';
 import FormRegister from '../components/Fragments/FormRegister';
 import AuthLayout from '../components/Layouts/AuthLayouts';
 
 const RegisterPage = () => {
   return (
-    <AuthLayout title="Register">
+    <AuthLayout title="Register" type="register">
       <FormRegister />
-      <p className="mt-5 text-center">
-       Have an account? <Link to="/login" className="font-bold text-blue-600">Sign in</Link>
-      </p>
     </AuthLayout>
   );
 };
