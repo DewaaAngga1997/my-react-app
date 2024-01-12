a6975ac8e40f125766bb9128f0f853888fea0233
Dewa Angga <devaangga53@gmail.com>
2024-01-12 21:09:46 +0800

    Membuat CartProduct menjadi nested component dan menggunakan props

--- src/components/Fragments/CartProduct.jsx
+++ src/components/Fragments/CartProduct.jsx
@@ -1,32 +1,58 @@
 import Button from "../Elements/Button"
-const CartProduct = () => {
+const CartProduct = (props) => {
+    const {children} = props
     return (
-        
-    <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow">
-                <a href="#">
-                    <img src="/images/shoes-1.jpg" 
-                    alt="product"
-                    className="p-8 rounded-t-lg" />
-                </a>
-                <div className="px-5 pb-5">
-                    <a href="#">
-                        <h5 className="text-xl font-semibold tracking-tight text-white">
-                        Sepatu baru
-                        </h5>
-                        <p className="text-m text-white">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus laborum, autem natus ea fugit deleniti illo quasi! Autem excepturi sapiente dicta dolorem! Impedit unde corrupti architecto? Tempore explicabo omnis qui.
-                        </p>
-                    </a>
-                </div>
-                    <div className="flex items-center justify-between px-5 pb-5">
-                        <span className="text-xl font-bold text-white">
-                        Rp 1.000.000
-                        </span>
-                        <Button variant="bg-blue-600">Add To Cart</Button>
-                    </div>
-            </div> 
-    )
-}
+    <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow">   
+    {children}           
+    </div> 
+    );
+};
+
+const Header = (props) => {
+    const {image} = props
+    return(
+    <a href="#">
+        <img src={image} 
+        alt="product"
+        className="p-8 rounded-t-lg" />
+    </a>
+    );
+};
+
+const Body = (props) => {
+    const {children, title} = props
+    return(
+        <div className="px-5 pb-5">
+            <a href="#">
+                <h5 className="text-xl font-semibold tracking-tight text-white">
+                {title}
+                </h5>
+                <p className="text-m text-white">
+                {children}
+                </p>
+            </a>
+        </div> 
+    );
+};
 
 
-export default CartProduct
+const Footer = (props) => {
+    const {price} = props
+    return(
+    <div className="flex items-center justify-between px-5 pb-5">
+        <span className="text-xl font-bold text-white">
+        {price}
+        </span>
+        <Button variant="bg-blue-600">
+            Add To Cart
+        </Button>
+    </div>
+    );
+};
+
+CartProduct.Header = Header
+CartProduct.Body = Body
+CartProduct.Footer = Footer
+
+
+export default CartProduct;
