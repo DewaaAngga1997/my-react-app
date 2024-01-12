055eb0e52f180b97ba2a265703e85196ade8a3fa
Dewa Angga <devaangga53@gmail.com>
2024-01-12 19:37:51 +0800

    Memindahkan cart ke file cartproduct.jsx

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,30 +1,8 @@
-import Button from "../components/Elements/Button"
+import CartProduct from "../components/Fragments/CartProduct"
 const ProductsPage = () => {
     return (
-        <div className="flex justify-center">
-            <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow">
-                <a href="#">
-                    <img src="/images/shoes-1.jpg" 
-                    alt="product"
-                    className="p-8 rounded-t-lg" />
-                </a>
-                <div className="px-5 pb-5">
-                    <a href="#">
-                        <h5 className="text-xl font-semibold tracking-tight text-white">
-                        Sepatu baru
-                        </h5>
-                        <p className="text-m text-white">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus laborum, autem natus ea fugit deleniti illo quasi! Autem excepturi sapiente dicta dolorem! Impedit unde corrupti architecto? Tempore explicabo omnis qui.
-                        </p>
-                    </a>
-                    <div className="flex items-center justify-between">
-                        <span className="text-3xl font-bold text-white">
-                        Rp 1.000.000
-                        </span>
-                        <Button variant="bg-blue-600">Add To Cart</Button>
-                    </div>
-                </div>
-            </div> 
+        <div className="flex justify-center py-5">
+            <CartProduct/>
         </div>
     )
 }
