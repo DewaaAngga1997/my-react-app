17414b9166feedf0ca3dfc015073711b468de903
Dewa Angga <devaangga53@gmail.com>
2024-01-12 21:12:36 +0800

    Menambah product baru

--- src/components/Fragments/CartProduct.jsx
+++ src/components/Fragments/CartProduct.jsx
@@ -2,7 +2,7 @@ import Button from "../Elements/Button"
 const CartProduct = (props) => {
     const {children} = props
     return (
-    <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow">   
+    <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2">   
     {children}           
     </div> 
     );
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -11,6 +11,15 @@ const ProductsPage = () => {
                 </CartProduct.Body>
                 <CartProduct.Footer price="Rp. 1.000.000" />
             </CartProduct>
+            <CartProduct>
+                <CartProduct.Header image ="/images/shoes-1.jpg" />
+                <CartProduct.Body title="Shoes Nike"> 
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero ipsam unde nesciunt, 
+                similique reprehenderit officia vel, sint, est hic minus aut error maxime illo animi
+                necessitatibus. Earum ipsa doloribus cum! 
+                </CartProduct.Body>
+                <CartProduct.Footer price="Rp. 1.000.000" />
+            </CartProduct>
         </div>
     )
 }
