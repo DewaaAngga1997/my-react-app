59d12f25db1d057e448572c6b38ac71798e1d18c
Dewa Angga <devaangga53@gmail.com>
2024-01-12 21:10:35 +0800

    menampilkan product dengan cara menggunkan props dan nested komponen yang dibuat

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -2,7 +2,15 @@ import CartProduct from "../components/Fragments/CartProduct"
 const ProductsPage = () => {
     return (
         <div className="flex justify-center py-5">
-            <CartProduct/>
+            <CartProduct>
+                <CartProduct.Header image ="/images/shoes-1.jpg" />
+                <CartProduct.Body title="Shoes Nike"> 
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero ipsam unde nesciunt, 
+                similique reprehenderit officia vel, sint, est hic minus aut error maxime illo animi
+                necessitatibus. Earum ipsa doloribus cum! 
+                </CartProduct.Body>
+                <CartProduct.Footer price="Rp. 1.000.000" />
+            </CartProduct>
         </div>
     )
 }
