881a2e845918ce7a21c01d1ccc624b19485d3c2b
Dewa Angga <devaangga53@gmail.com>
2024-01-12 19:22:48 +0800

    Membuat Cart di halaman product

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,7 +1,30 @@
+import Button from "../components/Elements/Button"
 const ProductsPage = () => {
     return (
-        <div>
-            Products Page
+        <div className="flex justify-center">
+            <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow">
+                <a href="#">
+                    <img src="/images/shoes-1.jpg" 
+                    alt="product"
+                    className="p-8 rounded-t-lg" />
+                </a>
+                <div className="px-5 pb-5">
+                    <a href="#">
+                        <h5 className="text-xl font-semibold tracking-tight text-white">
+                        Sepatu baru
+                        </h5>
+                        <p className="text-m text-white">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus laborum, autem natus ea fugit deleniti illo quasi! Autem excepturi sapiente dicta dolorem! Impedit unde corrupti architecto? Tempore explicabo omnis qui.
+                        </p>
+                    </a>
+                    <div className="flex items-center justify-between">
+                        <span className="text-3xl font-bold text-white">
+                        Rp 1.000.000
+                        </span>
+                        <Button variant="bg-blue-600">Add To Cart</Button>
+                    </div>
+                </div>
+            </div> 
         </div>
     )
 }
