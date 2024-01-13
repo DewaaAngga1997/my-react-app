5f245270b36d8e8055875ec535025f11a4ca6f80
Dewa Angga <devaangga53@gmail.com>
2024-01-13 17:03:23 +0800

    Menerapkan Rendering list map

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,27 +1,47 @@
 import CartProduct from "../components/Fragments/CartProduct";
+
+const products = [
+  {
+    id: 1,
+    name: "Nike shoes",
+    price: "Rp. 1.000.000",
+    image: "/images/shoes-1.jpg",
+    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero ipsam
+    unde nesciunt, similique reprehenderit officia vel, sint, est hic
+    minus aut error maxime illo animi necessitatibus. Earum ipsa doloribus
+    cum!`,
+  },
+  {
+    id: 2,
+    name: "Adidas shoes",
+    price: "Rp. 1.500.000",
+    image: "/images/shoes-1.jpg",
+    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero ipsam
+    unde nesciunt, similique reprehenderit officia vel, sint, est hic
+    minu`,
+  },
+  {
+    id: 3,
+    name: "Adidas kawe",
+    price: "Rp. 500.000",
+    image: "/images/shoes-1.jpg",
+    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero ipsam
+    unde nesciunt`,
+  },
+];
+
 const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CartProduct>
-        <CartProduct.Header image="/images/shoes-1.jpg" />
-        <CartProduct.Body title="Shoes Nike">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero ipsam
-          unde nesciunt, similique reprehenderit officia vel, sint, est hic
-          minus aut error maxime illo animi necessitatibus. Earum ipsa doloribus
-          cum!
-        </CartProduct.Body>
-        <CartProduct.Footer price="Rp. 1.000.000" />
-      </CartProduct>
-      <CartProduct>
-        <CartProduct.Header image="/images/shoes-1.jpg" />
-        <CartProduct.Body title="Shoes Nike">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero ipsam
-          unde nesciunt, similique reprehenderit officia vel, sint, est hic
-          minus aut error maxime illo animi necessitatibus. Earum ipsa doloribus
-          cum!
-        </CartProduct.Body>
-        <CartProduct.Footer price="Rp. 1.000.000" />
-      </CartProduct>
+      {products.map((product) => (
+        <CartProduct>
+          <CartProduct.Header image={product.image} />
+          <CartProduct.Body title={product.name}>
+            {product.description}
+          </CartProduct.Body>
+          <CartProduct.Footer price={product.price} />
+        </CartProduct>
+      ))}
     </div>
   );
 };
