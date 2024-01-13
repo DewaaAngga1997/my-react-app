6e457388b941cd5d096cb3f7bbd53e3ef452f0cf
Dewa Angga <devaangga53@gmail.com>
2024-01-13 17:40:23 +0800

    menambahkan key di product.map

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -34,7 +34,7 @@ const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
       {products.map((product) => (
-        <CartProduct>
+        <CartProduct key={product.id}>
           <CartProduct.Header image={product.image} />
           <CartProduct.Body title={product.name}>
             {product.description}
