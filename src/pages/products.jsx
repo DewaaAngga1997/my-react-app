1180d61ba0cb6ee9d5901af505130bc12ba64a7d
Dewa Angga <devaangga53@gmail.com>
2024-01-13 19:18:34 +0800

    Membuat navbar dan membuat fungsi button hendle logout

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,4 +1,6 @@
+import { Fragment } from "react";
 import CartProduct from "../components/Fragments/CartProduct";
+import Button from "../components/Elements/Button";
 
 const products = [
   {
@@ -30,19 +32,37 @@ const products = [
   },
 ];
 
+//membuat variabel untuk memanggil di localstorage
+const email = localStorage.getItem("email");
+
 const ProductsPage = () => {
+  //membuat fungsi hendle Logout
+  const handleLogout = () => {
+    //menghapus data di localstorage email dan password
+    localStorage.removeItem("email");
+    localStorage.removeItem("password");
+    window.location.href = "/login";
+  };
   return (
-    <div className="flex justify-center py-5">
-      {products.map((product) => (
-        <CartProduct key={product.id}>
-          <CartProduct.Header image={product.image} />
-          <CartProduct.Body title={product.name}>
-            {product.description}
-          </CartProduct.Body>
-          <CartProduct.Footer price={product.price} />
-        </CartProduct>
-      ))}
-    </div>
+    <Fragment>
+      <div className="flex justify-end h-20 bg-blue-600 text-white items-center px-10">
+        {email}
+        <Button variant="bg-red-600 ml-5" onClick={handleLogout}>
+          Logout
+        </Button>
+      </div>
+      <div className="flex justify-center py-5">
+        {products.map((product) => (
+          <CartProduct key={product.id}>
+            <CartProduct.Header image={product.image} />
+            <CartProduct.Body title={product.name}>
+              {product.description}
+            </CartProduct.Body>
+            <CartProduct.Footer price={product.price} />
+          </CartProduct>
+        ))}
+      </div>
+    </Fragment>
   );
 };
 
