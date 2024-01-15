9ac187895fa38450a9ac4313fc99c92858a76057
Dewa Angga PC <devaangga53@gmail.com>
2024-01-15 12:24:48 +0800

    Membuat tabel cart dan membuat fungsi qty agar produk yang sama tidak muncul dua kali agar qty nya saja yang bertambah

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -17,7 +17,7 @@ const products = [
   {
     id: 2,
     name: "Adidas shoes",
-    price: 15000000,
+    price: 1500000,
     image: "/images/shoes-1.jpg",
     description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero ipsam
     unde nesciunt, similique reprehenderit officia vel, sint, est hic
@@ -41,10 +41,22 @@ const ProductsPage = () => {
   //state selalu berpasangan ada state dan update'an statenya
   const [cart, setCart] = useState([
     {
-      name: "Sepatu lama",
+      id: 1,
       qty: 1,
     },
   ]);
+
+  const handleAddToCart = (id) => {
+    if (cart.find((item) => item.id === id)) {
+      setCart(
+        cart.map((item) =>
+          item.id === id ? { ...item, qty: item.qty + 1 } : item
+        )
+      );
+    } else {
+      setCart([...cart, { id, qty: 1 }]);
+    }
+  };
 
   //membuat fungsi hendle Logout
   const handleLogout = () => {
@@ -62,28 +74,64 @@ const ProductsPage = () => {
         </Button>
       </div>
       <div className="flex justify-center py-5">
-        <div className="h-3/4 flex flex-wrap">
+        <div className="w-4/6 flex flex-wrap">
           {products.map((product) => (
             <CartProduct key={product.id}>
               <CartProduct.Header image={product.image} />
               <CartProduct.Body title={product.name}>
                 {product.description}
               </CartProduct.Body>
-              <CartProduct.Footer price={product.price} />
+              <CartProduct.Footer
+                price={product.price}
+                id={product.id}
+                handleAddToCart={handleAddToCart}
+              />
             </CartProduct>
           ))}
         </div>
-        <div className="w-1/4">
-          <h1 className="text-3xl text-blue-600 font-bold ">Cart</h1>
-          <ul>
-            {cart.map((item) => (
-              <li key={item.name}>{item.name}</li>
-            ))}
-          </ul>
+        <div className="w-2/6">
+          <h1 className="text-3xl text-blue-600 font-bold ml-5 mb-2">Cart</h1>
+          <table className="text-left table-auto border-separate border-spacing-x-5">
+            <thead>
+              <tr>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cart.map((item) => {
+                const product = products.find(
+                  (product) => product.id === item.id
+                );
+                return (
+                  <tr key={item.id}>
+                    <td>{product.name}</td>
+                    <td>
+                      {product.price.toLocaleString("id-ID", {
+                        style: "currency",
+                        currency: "IDR",
+                        minimumFractionDigits: 0,
+                      })}
+                    </td>
+                    <td className="text-center">{item.qty}</td>
+                    <td>
+                      {(product.price * item.qty).toLocaleString("id-ID", {
+                        style: "currency",
+                        currency: "IDR",
+                        minimumFractionDigits: 0,
+                      })}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
       </div>
-      <div className="flex w-100 justify-center">
-        {/*componen counter di bawah menggunakan class componen */}
+      <div className="flex justify-center w-4/6">
+        {/* membuat komponen menggunakan class tapi tidak di sarankan */}
         <Counter></Counter>
       </div>
     </Fragment>
