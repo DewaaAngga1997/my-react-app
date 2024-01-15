49b711d7a5136d69f219a6733f6fa0d5d0daba85
Dewa Angga PC <devaangga53@gmail.com>
2024-01-15 11:07:59 +0800

    Menambahkan format rupiah kedalam price

--- src/components/Fragments/CartProduct.jsx
+++ src/components/Fragments/CartProduct.jsx
@@ -35,7 +35,13 @@ const Footer = (props) => {
   const { price } = props;
   return (
     <div className="flex items-center justify-between px-5 pb-5">
-      <span className="text-xl font-bold text-white">{price}</span>
+      <span className="text-xl font-bold text-white">
+        {price.toLocaleString("id-ID", {
+          style: "currency",
+          currency: "IDR",
+          minimumFractionDigits: 0,
+        })}
+      </span>
       <Button variant="bg-blue-600">Add To Cart</Button>
     </div>
   );
