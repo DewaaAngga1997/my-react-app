43fbc08fcaf84e4e67707c8062a9bf5e12ed1f5c
Dewa Angga PC <devaangga53@gmail.com>
2024-01-17 12:06:51 +0800

    Belajar memahami DidMount dan DidUpdate

--- src/components/Fragments/Counter.jsx
+++ src/components/Fragments/Counter.jsx
@@ -2,9 +2,25 @@ import React from "react";
 class Counter extends React.Component {
   constructor(props) {
     super(props);
+    //code di bawah set nilai count = 0
     this.state = {
       count: 0,
     };
+    console.log("constructor");
+  }
+
+  componentDidMount() {
+    //code di bawah set nilai count = 1 mengubah nilai dari state awal yaitu count = 0
+    this.setState({ count: 0 });
+    console.log("componentDidMount");
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+    //code di bawah set nilai count = 0 jika nilai count yang sudah sampai 0 maka akan di kembalikan lagi menjadi 0
+    if (this.state.count === 10) {
+      this.setState({ count: 0 });
+    }
   }
   render() {
     return (
@@ -16,6 +32,7 @@ class Counter extends React.Component {
         >
           +
         </button>
+        {console.log("render")}
       </div>
     );
   }
