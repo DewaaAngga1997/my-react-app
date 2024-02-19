c055d37822d25a7671992b5d8e6a1a6fb62ec449
Dewa Angga PC <devaangga53@gmail.com>
2024-02-19 22:24:45 +0800

    auth process

--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -1,37 +1,49 @@
 import InputForm from "../Elements/Input/index";
 import Button from "../Elements/Button/index";
 import { useEffect, useRef } from "react";
+import { login } from "../../services/auth.service";
 
 const FormLogin = () => {
   //membuat fungsi handleLogin
   const handleLogin = (event) => {
     //event.preventdefault digunakan agar tidak terjadi refresh
     event.preventDefault();
-    //target.email di ambil dari name email di input form dan disimpan ke localStorage
-    localStorage.setItem("email", event.target.email.value);
-    //target.password di ambil dari name password di input form dan disimpan ke localStorage
-    localStorage.setItem("password", event.target.password.value);
-    //redirect ke halaman product
-    window.location.href = "/products";
+    const data = {
+      username: event.target.username.value,
+      password: event.target.password.value,
+    };
+    login(data, (status, res) => {
+      if (status) {
+        localStorage.setItem("token", res);
+      } else {
+        console.log(res);
+      }
+    });
+    // //target.email di ambil dari name email di input form dan disimpan ke localStorage
+    // localStorage.setItem("email", event.target.email.value);
+    // //target.password di ambil dari name password di input form dan disimpan ke localStorage
+    // localStorage.setItem("password", event.target.password.value);
+    // //redirect ke halaman product
+    // window.location.href = "/products";
   };
 
   //kita juga bisa menggunakan useRef untuk mumbuat focus pada from input login
   //(saat di halaman login kursor akan otomatis berada di input email)
   //dengan cara membuat funtion di bawah
-  const emailRef = useRef(null);
+  const usernameRef = useRef(null);
   // lalu menggunakan useEffect
   useEffect(() => {
-    emailRef.current.focus();
+    usernameRef.current.focus();
   }, []);
 
   return (
     <form onSubmit={handleLogin}>
       <InputForm
-        label="Email"
-        type="email"
-        placeholder="example@mail.com"
-        name="email"
-        ref={emailRef}
+        label="Username"
+        type="text"
+        placeholder="dewa angga"
+        name="username"
+        ref={usernameRef}
       ></InputForm>
 
       <InputForm
